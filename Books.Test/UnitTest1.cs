7c30fe40614064f92f752b86acd0abe810c689c0
ravigaikawad6294 <ravigaikawad6294@gmail.com>
2018-10-27 12:30:57 +0530

    Unit Testing

---
+++ Books.Test/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using BooksAPI.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Books.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var controller = new BooksController();
+
+            var result = controller.GetBook(1);
+            Assert.IsNotNull(result);
+        }
+    }
+}
