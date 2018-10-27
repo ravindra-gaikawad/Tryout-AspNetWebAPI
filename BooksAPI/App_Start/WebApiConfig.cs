856dab51860545fc9c3e661c97813aed548edbb3
ravigaikawad6294 <ravigaikawad6294@gmail.com>
2018-10-27 12:03:18 +0530

    Tracing in Web API 2

--- BooksAPI/App_Start/WebApiConfig.cs
+++ BooksAPI/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Tracing;
 
 namespace BooksAPI
 {
@@ -10,6 +11,9 @@ namespace BooksAPI
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            SystemDiagnosticsTraceWriter traceWriter = config.EnableSystemDiagnosticsTracing();
+            traceWriter.IsVerbose = true;
+            traceWriter.MinimumLevel = TraceLevel.Debug;
 
             // Web API routes
             config.MapHttpAttributeRoutes();
