aae837adf5cf660f8e4b67353d0a0ae137b5bf0b
ravindra-gaikawad <ravigaikawad6294@gmail.com>
2018-10-29 12:20:51 +0530

    CRUD Logic Using Entity Framework

---
+++ WEB-API-CRUD-Logics/App_Start/WebApiConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace WEB_API_CRUD_Logics
+{
+    /// <summary>
+    /// Reference: https://www.c-sharpcorner.com/article/asp-net-web-api-crud-logics-using-entity-framework-without-writing-single-code/
+    /// </summary>
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.EnableCors(new EnableCorsAttribute("*", "*", "GET,PUT,POST,DELETE"));
+        }
+    }
+}
---
+++ WEB-API-CRUD-Logics/Controllers/CRUDController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WEB_API_CRUD_Logics.Models;
+
+namespace WEB_API_CRUD_Logics.Controllers
+{
+    //[RoutePrefix("api/employee")]
+    public class CRUDController : ApiController
+    {
+        private WEB_API_CRUD_LogicsContext db = new WEB_API_CRUD_LogicsContext();
+
+        // GET: api/CRUD
+        //[Route("")]
+        public IQueryable<Employee> GetEmployees()
+        {
+            return db.Employees;
+        }
+
+        // GET: api/CRUD/5
+        //[Route("{id}")]
+        [ResponseType(typeof(Employee))]
+        public IHttpActionResult GetEmployee(long id)
+        {
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(employee);
+        }
+
+        // PUT: api/CRUD/5
+        //[Route("{id}")]
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutEmployee(long id, Employee employee)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != employee.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(employee).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmployeeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/CRUD
+        //[Route("")]
+        [ResponseType(typeof(Employee))]
+        public IHttpActionResult PostEmployee(Employee employee)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Employees.Add(employee);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = employee.ID }, employee);
+        }
+
+        // DELETE: api/CRUD/5
+        //[Route("{id}")]
+        [ResponseType(typeof(Employee))]
+        public IHttpActionResult DeleteEmployee(long id)
+        {
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+
+            return Ok(employee);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool EmployeeExists(long id)
+        {
+            return db.Employees.Count(e => e.ID == id) > 0;
+        }
+    }
+}
